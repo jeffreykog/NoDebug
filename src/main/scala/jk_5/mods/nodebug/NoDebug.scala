81d7d001f80ee8370a0881ad721ed6d69553b738
jk-5 <jeffreykog@me.com>
2015-05-08 13:35:27 +0200

    Updated to 1.7.10

--- src/main/scala/jk_5/mods/nodebug/NoDebug.scala
+++ src/main/scala/jk_5/mods/nodebug/NoDebug.scala
@@ -7,7 +7,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent
 import cpw.mods.fml.common.gameevent.TickEvent
 import net.minecraft.client.Minecraft.{getMinecraft => mc}
 
-@Mod(modid = "NoDebug", version = "1.0", modLanguage = "scala")
+@Mod(modid = "NoDebug", version = "1.0.1", modLanguage = "scala")
 object NoDebug {
   @EventHandler def preInit(event: FMLPreInitializationEvent){
     if(event.getSide.isClient) FMLCommonHandler.instance.bus.register(this)
